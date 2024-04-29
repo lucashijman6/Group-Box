2f04bb83983850810313786a6a4082bf98805830
Lucas Hijman <lucas.hijman@linden-it.com>
2024-04-29 20:22:39 +0200

    Put initial files in project

---
+++ typings/GroupBoxProps.d.ts
@@ -0,0 +1,24 @@
+/**
+ * This file was generated from GroupBox.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix Widgets Framework Team
+ */
+import { CSSProperties } from "react";
+
+export interface GroupBoxProps<Style> {
+    name: string;
+    style: Style[];
+    yourName: string;
+}
+
+export interface GroupBoxPreviewProps {
+    /**
+     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
+     */
+    className: string;
+    class: string;
+    style: string;
+    styleObject?: CSSProperties;
+    readOnly: boolean;
+    yourName: string;
+}
